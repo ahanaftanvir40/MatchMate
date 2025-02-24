0b6c6cfe937637c2ff0c0bcefa7e74599694fd9c
Ahanaf Tanvir <111105281+ahanaftanvir40@users.noreply.github.com>
2025-02-25 01:53:12 +0600

    fixed auth middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,10 +2,11 @@ import UserModel from "../models/userModel.js";
 import jwt from 'jsonwebtoken';
 
 export async function authToken(req, res, next) {
-    const token = req.headers.authorization.split(' ')[1];
-    if (!token) {
-        return res.status(401).json({ success: false, message: 'Unauthorized Please SignIn' });
+    const authHeader = req.headers.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ success: false, message: 'Unauthorized. Please Sign In' });
     }
+    const token = req.headers.authorization.split(' ')[1];
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await UserModel.findOne({ _id: decoded.value });
